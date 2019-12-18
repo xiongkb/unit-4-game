0cb5785506dd75beb83e4d3a3b69a21e7f4da62d
xiongkb <mkan-bx@hotmail.com>
2019-12-17 21:20:49 -0800

    pictures' hidden value set

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,51 @@
 // variables to be listed
-var ranNum  //will add 19 so it starts at 19. random number for "gen-num"
-var gemNum = Math.round(Math.random()*12)+1; // hidden gem value "gem-value"
+var ranNum = Math.floor(Math.random()*102)+19;  //will add 19 so it starts at 19. random number for "gen-num"
+var gemCounter = 0;
+var wins = 0;
+var loss = 0;
+
+// hidden gem value "gem-value"
+var blueNum = Math.floor(Math.random()*12)+1;
+var violetNum = Math.floor(Math.random()*12)+1;
+var redNum = Math.floor(Math.random()*12)+1;
+var greenNum = Math.floor(Math.random()*12)+1;
 
 // showing the randomNUmber that player needs to match
 function start() {
-    ranNum = Math.round(Math.random()*102)+19;
     $(".gen-num").text(ranNum);
+    $(".blue").attr("blueNum", blueNum);
+    $(".violet").attr("violetNum", violetNum);
+    $(".red").attr("redNum", redNum);
+    $(".green").attr("greenNum", greenNum);
 }
 start();
 
-function blue () {
+// making it so that each picture when pressed will add to the collected counter
+$(".blue").on("click", function() {
+    var gemValue = $(this).attr("blueNum");
+    gemValue = parseInt(gemValue);
+    gemCounter += gemValue;
+    $(".gem-value").text(gemCounter);
+})
+$(".violet").on("click", function() {
+    var gemValue = $(this).attr("violetNum");
+    gemValue = parseInt(gemValue);
+    gemCounter += gemValue;
+    $(".gem-value").text(gemCounter);
+})
+$(".red").on("click", function() {
+    var gemValue = $(this).attr("redNum");
+    gemValue = parseInt(gemValue);
+    gemCounter += gemValue;
+    $(".gem-value").text(gemCounter);
+})
+$(".green").on("click", function() {
+    var gemValue = $(this).attr("greenNum");
+    gemValue = parseInt(gemValue);
+    gemCounter += gemValue;
+    $(".gem-value").text(gemCounter);
+})
 
-}
+if (gemCounter === ranNum) {
 
-function violet () {
-    
-}
-
-function red () {
-    
-}
-
-function green () {
-    
 }
